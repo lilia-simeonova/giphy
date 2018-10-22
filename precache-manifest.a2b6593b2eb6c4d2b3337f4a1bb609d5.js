0b95a61a275c46b71001b9b79d05dc4e52af010e
Lilia Simeonova <lilia.valentinova.simeonova@gmail.com>
2018-10-22 10:16:55 -0700

    Updates

---
+++ precache-manifest.a2b6593b2eb6c4d2b3337f4a1bb609d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8247f78cc0bb3dc2636b",
+    "url": "/giphy/static/js/runtime~main.8247f78c.js"
+  },
+  {
+    "revision": "aa86affe67db9e7b55fe",
+    "url": "/giphy/static/js/main.aa86affe.chunk.js"
+  },
+  {
+    "revision": "47712619ecda1c210b8e",
+    "url": "/giphy/static/js/1.47712619.chunk.js"
+  },
+  {
+    "revision": "aa86affe67db9e7b55fe",
+    "url": "/giphy/static/css/main.248de928.chunk.css"
+  },
+  {
+    "revision": "77fffc7909bc5e722d47907c1f9e2f48",
+    "url": "/giphy/index.html"
+  }
+];
