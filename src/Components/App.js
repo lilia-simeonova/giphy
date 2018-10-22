7f295567c27b3226632dfb7f814e0596b0eb1f6c
Lilia Simeonova <lilia.valentinova.simeonova@gmail.com>
2018-10-22 10:12:45 -0700

    Update base URL

--- src/Components/App.js
+++ src/Components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <nav className="nav">
             <div className="title">Giphy</div>
